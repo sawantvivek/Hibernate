03eb0be16d8ad84d1e94e46bc814c9992bf37ca3
vivek <vivek.sawant@skillnetinc.com>
2015-01-05 14:04:53 +0530

    Spring MessageBean and Context.

--- SpringWithMaven/src/main/java/org/vivek/springexample/Circle.java
+++ SpringWithMaven/src/main/java/org/vivek/springexample/Circle.java
@@ -1,13 +1,33 @@
 package org.vivek.springexample;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
 
 public class Circle implements Shape {
+	
+	
 
 	public Point center;
-	
+
 	public int centervalue = 0;
+
+	@Autowired
+	public MessageSource messageSource;
+	
+	
+	
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	public int getCentervalue() {
 		return centervalue;
@@ -22,14 +42,27 @@ public class Circle implements Shape {
 	}
 
 	@Autowired
-	@Qualifier("CircleRelated")
+	@Qualifier(value="CircleRelated")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
 
 	public void draw() {
-		System.out.println("Circle: Center point X: " + center.getX()
-				+ " and Y : " + center.getY());
+		
+		System.out.println(this.getMessageSource().getMessage("display.circle", null, "Default Circle", null));
+		System.out.println(this.getMessageSource().getMessage("display.point", new Object []{center.getX(),center.getY()}, "Default Circle", null));
+		
+		//System.out.println("Circle: Center point X: " + center.getX() + " and Y : " + center.getY());
+	}
+
+	@PostConstruct
+	public void initalizeCircle() {
+		System.out.println("initialize Circle");
+	}
+
+	@PreDestroy
+	public void destroyCircle() {
+		System.out.println("Destroy Circle");
 	}
 
 }
--- SpringWithMaven/src/main/java/org/vivek/springexample/ShapeApp.java
+++ SpringWithMaven/src/main/java/org/vivek/springexample/ShapeApp.java
@@ -1,5 +1,7 @@
 package org.vivek.springexample;
 
+import java.util.Locale;
+
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,9 +12,10 @@ public class ShapeApp {
 
 	public static void main(String[] args) {
 		context = new ClassPathXmlApplicationContext("spring.xml");
-		context.refresh();
 		Shape shape = (Shape) context.getBean("Circle");
 		shape.draw();
+		
+		System.out.println(context.getMessage("greeting", null, "default hello !!!", Locale.UK));
 	}
 
 }
