532d6c710c2bec55537ee889aebc2b0264dc583e
vivek <vivek.sawant@skillnetinc.com>
2015-01-05 17:18:39 +0530

    Writing Our First Aspect Part 2

---
+++ SpringWithMaven/src/main/java/org/vivek/springexample/aspect/LoggingAspect.java
@@ -0,0 +1,13 @@
+package org.vivek.springexample.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+
+	@Before("execution(public String getName())")
+	public void loggingAction(){
+		System.out.println("Print logging action");
+	}
+}
